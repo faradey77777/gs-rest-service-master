02428c74086e2da47d3654f3d464fd892ff258bb
faradey77777 <Faradey11061992>
2020-03-15 21:38:10 +0200

    put/post add

---
+++ src/main/java/com/example/restservice/Controller.java
@@ -0,0 +1,34 @@
+package com.example.restservice;
+
+import com.example.restservice.domain.Message;
+import com.example.restservice.repo.MessegeRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class Controller {
+    @Autowired
+    private MessegeRepo messegeRepo;
+
+    @GetMapping("/{id}")
+    public Message getMessege(@PathVariable Integer id) {
+        return messegeRepo.findById(id).orElse(null);
+    }
+
+    @PostMapping @PutMapping("/{name}")
+    public Message mapping(@RequestBody String name) {
+        Message message = new Message();
+        message.setText(name);
+        messegeRepo.save(message);
+        return message;
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public void deleteUser(@RequestBody Integer id) {
+        Message message1 = messegeRepo.findById(id).orElse(null);
+        messegeRepo.deleteById(id);
+    }
+
+}
--- src/main/java/com/example/restservice/repo/MessegeRepo.java
+++ src/main/java/com/example/restservice/repo/MessegeRepo.java
@@ -3,10 +3,9 @@ package com.example.restservice.repo;
 import com.example.restservice.domain.Message;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface MessegeRepo extends JpaRepository<Message, Integer> {
 
 
+//    Message getByText();
 
 }
